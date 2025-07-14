11842b12b47336e542141b04801e7f77ad88004a
varshedhaa-07 <153357408+varshedhaa-07@users.noreply.github.com>
2025-07-14 14:09:42 +0530

    Delete task directory

--- task/src/main/java/com/example/studentspringproject/models/StudentDetails.java
+++
@@ -1,8 +0,0 @@
-package com.example.studentspringproject.models;
-
-import lombok.Data;
-
-@Data
-public class StudentDetails {
-
-}
--- task/src/main/java/com/example/studentspringproject/repository/StudentRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.studentspringproject.repository;
-
-import com.example.studentspringproject.models.Student;
-import org.springframework.stereotype.Repository;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-@Repository
-public interface StudentRepository extends JpaRepository<Student,Integer>{
-}
